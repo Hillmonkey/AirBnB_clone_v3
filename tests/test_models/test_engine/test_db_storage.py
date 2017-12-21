ed7313a3be2fea2a197698b27dc0321c50b2b6a1
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-21 23:30:04 +0000

    added get() and count() for DBStorage & the corresponding test cases

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,7 +3,6 @@
 testing methods for dbstorage"""
 
 
-#from models.engine import DBStorage
 from models.engine import db_storage
 from models.base_model import Base
 from models.amenity import Amenity
@@ -14,13 +13,15 @@ from models.review import Review
 from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+import inspect
 import json
 import os
 import pep8
 import unittest
 import sqlalchemy
 
-name2class = {
+DBStorage = db_storage.DBStorage
+classes = {
     'Amenity': Amenity,
     'City': City,
     'Place': Place,
@@ -30,19 +31,49 @@ name2class = {
 }
 
 
-class TestDBFormatting(unittest.TestCase):
-    """class: TestDBFormatting
-    tests for pep8 compliance and existence of doc strings"""
+class TestDBStorageDocs(unittest.TestCase):
+    """Tests to check the documentation and style of DBStorage class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.fs_f = inspect.getmembers(DBStorage, inspect.isfunction)
 
-    def TestPep8(self):
-        """method: TestPep8
-        tests for pep8 compliance of DBStorage"""
-        pass
+    def test_pep8_conformance_db_storage(self):
+        """Test that models/engine/db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def TestDocStrings(self):
-        """method: TestDocStrings
-        tests for existence of doc strings"""
-        pass
+    def test_pep8_conformance_test_db_storage(self):
+        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_engine/\
+test_db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_db_storage_module_docstring(self):
+        """Test for the db_storage.py module docstring"""
+        self.assertIsNot(db_storage.__doc__, None,
+                         "db_storage.py needs a docstring")
+        self.assertTrue(len(db_storage.__doc__) >= 1,
+                        "db_storage.py needs a docstring")
+
+    def test_db_storage_class_docstring(self):
+        """Test for the DBStorage class docstring"""
+        self.assertIsNot(DBStorage.__doc__, None,
+                         "DBStorage class needs a docstring")
+        self.assertTrue(len(DBStorage.__doc__) >= 1,
+                        "DBStorage class needs a docstring")
+
+    def test_fs_func_docstrings(self):
+        """Test for the presence of docstrings in DBStorage methods"""
+        for func in self.fs_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
 
 
 class TestDBStorage(unittest.TestCase):
@@ -50,26 +81,43 @@ class TestDBStorage(unittest.TestCase):
     all tests to run on Database Storage"""
 
     def setUp(self):
-        """method: setUp
-        connect to database before running tests in this class"""
-        self.session = db_storage.DBStorage()
+        '''connect & reload the database'''
+        self.storage = db_storage.DBStorage()
+        # how come the reload() in models/__init__ doesn't take care of it?
+        self.storage.reload()
 
     def tearDown(self):
-        """method: tearDown
-        shut down database connection"""
-        self.session.close()
+        '''terminate database connection'''
+        self.storage.close()
 
     def test_get(self):
-        """method: test_get
-        database copy of new object == copy via get"""
-        new_obj = Amenity()
-        print()
-        print("++++++++++ db-test-get ++++++++++")
-        print("new_obj = {}".format(new_obj))
-        self.session.new(new_obj)
-        output = self.session.get("Amenity", new_obj.id)
-        print()
-        print("++++ Larry test +++++++++++")
-        print("type(self.session) = {}".format(type(self.session)))
-        print("type(new_obj) = {}".format(type(new_obj)))
-        self.assertEqual(5, 4)
+        '''test that get returns object based on cls name & id
+        database copy of new object == copy via get'''
+        new_obj = State(name="Test get State")
+        self.storage.new(new_obj)
+        self.storage.save()
+        output = self.storage.get("State", new_obj.id)
+        self.assertIs(new_obj, output)
+
+    def test_count(self):
+        '''test that count returns the number of objects based on clsname,
+        or number of all objects if cls=None'''
+        new_obj = State(name="Test count State")
+        self.storage.new(new_obj)
+        self.storage.save()
+
+        state_count = self.storage.count("State")
+        print("state count returned by count():", state_count)
+        state_num_query = self.storage._DBStorage__session.query(State).count()
+#        print("type of:", type(storage._DBStorage__session))
+#        print("state count returned by query:", state_count)
+        self.assertEqual(state_count, state_num_query)
+
+        all_count = self.storage.count()
+#        print("all object count returned by count():", all_count)
+        obj_num_query = 0
+        for key, value in classes.items():
+            obj_num_query += self.storage._DBStorage__session.query(
+                value).count()
+#        print("all object count returned by query:", obj_num_query)
+        self.assertEqual(all_count, obj_num_query)
