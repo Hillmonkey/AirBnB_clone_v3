fab132f3fb61294d87decfdf92679a6dc8f542ce
Larry Madeo <lmadeo76@gmail.com>
2017-12-22 02:04:41 +0000

    removed debugging print statements

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
@@ -12,9 +13,11 @@ app.register_blueprint(app_views)
 def teardown_close(exception):
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(error="Not found"), 404
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
-    print(type(host))
-    print(type(port))
     app.run(host=host, port=port)
