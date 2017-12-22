ab5859fd6134e76b04752479fa50fafb4aab8c4b
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-22 01:41:10 +0000

    added condition for when the database will be used for testing in the __init__.py

---
+++ tests/test_models/test_engine/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""initialize the models.engine package"""
+
+from os import getenv
+
+# DBSTORAGE
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+
+# FILESTORAGE
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
